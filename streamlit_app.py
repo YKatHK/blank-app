5e1fc9da495357bd272ecad629fc0aa17cd93b8f
YK <yinkieyao@gmail.com>
2024-08-18 17:02:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hi. It's YK. Welocme to my note space.")
+st.title("Hi. It's YK. Welcome to my note space.")
 st.write(
     "In the note space, I'd like to share my investment strategy."
 )
