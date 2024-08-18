569db15ff35e558958c60049bbe8384803a5102f
YK <yinkieyao@gmail.com>
2024-08-18 17:14:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("Hi. It's YK. Welcome to my note space.")
+st.title("Hi. It's YK.")
 st.write(
-    "In the note space, I'd like to share my investment strategy."
+    "Welcome to my note space."
 )
+
+st.markdown("The ==Valuation== **Frame**: PB.ROE.")
+
+st.markdown('''
+    :$$P_{t}=B_{t}\cdot(\frac{P}{B})_{t}$$
+    ''')
