37bd4cefd7d7d5c8e3c605f5f7cba7906ebb9cd2
YK <yinkieyao@gmail.com>
2024-08-19 08:30:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,30 +5,16 @@ def blogPath(path_str='blog'):
     streamlit_path = Path(".").absolute()
     blog_path = streamlit_path.joinpath(path_str)
     return blog_path
-    
-st.title("Hi. It's YK.")
-st.write(
-    "Welcome to my note space."
-)
-
-st.markdown("The :orange[Valuation] **Frame**: PB.ROE.")
-
-st.markdown(r'''
-        $$P_{t}=B_{t}\cdot(\frac{P}{B})_{t} $$
-        ''')
-
-st.markdown(r'''
-        $$\Delta P_{t} = \Delta B_{t}\cdot(\frac{P}{B})_{t} + B_{t}\cdot\Delta(\frac{P}{B})_{t} + \Delta B_{t}\cdot\Delta(\frac{P}{B})_{t}$$
-        ''')
 
 # Create a sidebar menu with different options
-menu = ["Home", "View Posts"]
+menu = ["Home", "View Posts", "Latex Example"]
 choice = st.sidebar.selectbox("Menu", menu)
 
 # Display the selected option
 if choice == "Home":
-    st.title("Welcome to my blog")
-    st.write("This is a simple blog app built with streamlit and python.")
+    st.title("Hi. It's YK.")
+    st.write(
+        "Welcome to my note space.\nThis is a simple blog app built with streamlit and python."
     st.write("Enjoy!")
 
 elif choice == "View Posts":
@@ -38,3 +24,14 @@ elif choice == "View Posts":
     with open(str(blog_file), 'r') as f:
         markdown_string = f.read()
     st.markdown(markdown_string)
+
+elif choice == "Latex Example":
+    st.title("Latex Example")
+    st.markdown("The :orange[Valuation] **Frame**: PB.ROE.")
+    st.markdown(r'''
+            $$P_{t}=B_{t}\cdot(\frac{P}{B})_{t} $$
+            ''')
+    st.markdown(r'''
+            $$\Delta P_{t} = \Delta B_{t}\cdot(\frac{P}{B})_{t} + B_{t}\cdot\Delta(\frac{P}{B})_{t} + \Delta B_{t}\cdot\Delta(\frac{P}{B})_{t}$$
+            ''')
+
